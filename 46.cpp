49eff5ad3e290a376876e5f8f1a6dddace90b40d
fash97 <qiaowanhui@gmail.com>
2017-07-31 11:35:27 +0800

    more

---
+++ 46.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<vector<int>> permute(vector<int>& nums) {
+      vector<vector<int>> result;
+      helper(result, nums, 0, nums.size()-1);
+      return result;
+    }
+
+    void helper(vector<vector<int>> &result, vector<int>& nums, int l, int r){
+      int i;
+      if(l == r){
+        result.push_back(nums);
+      }
+      else{
+        for (i = l; i <= r; i++){
+          swap(nums[l], nums[i]);
+          helper(result, nums, l+1, r);
+          swap(nums[l], nums[i]);
+        }
+      }
+    }
+};
